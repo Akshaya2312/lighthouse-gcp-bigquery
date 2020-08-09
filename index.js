95a945cb24a895e49b2aee2032f977ee6a2239b2
Chitrang Natu <chit786@gmail.com>
2020-08-09 21:53:45 +0200

    feat: upload to GCP

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const path = require('path');
 const fs = require('fs');
 const converter = require('json-2-csv');
+const { Storage } = require('@google-cloud/storage');
+
+const GOOGLE_CLOUD_PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT_ID;
+const GOOGLE_CLOUD_KEYFILE = process.env.GOOGLE_CLOUD_KEYFILE;
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+const storage = new Storage({
+  projectId: GOOGLE_CLOUD_PROJECT_ID,
+  keyFilename: GOOGLE_CLOUD_KEYFILE,
+});
+
+const filename = `./pageSummary_${Date.now()}.csv`;
 
 module.exports = {
   concurrency: 1,
@@ -11,21 +23,28 @@ module.exports = {
   open(context, options) {
     this.log = context.intel.getLogger('sitespeedio.plugin.lighthouse-gcp-bigquery');
   },
-  processMessage(message, queue) {
+
+  async processMessage(message, queue) {
     const log = this.log;
     switch(message.type) {
-        case 'lighthouse.pageSummary': {
-          converter.json2csvAsync(message).then(csv => {
-            fs.writeFileSync('lighthouse.pageSummary.csv', csv);
-            log.info('lighthouse.pagesummary.csv captured');
-          }).catch(err => {
-            log.error('lighthouse.pagesummary.csv not captured', err)
-          });
-          break;
-        }
+      case 'lighthouse.pageSummary': {
+        await converter.json2csvAsync(message).then(csv => {
+          fs.writeFileSync(filename, csv);
+          log.info(`${filename} captured`);
+        }).catch(err => {
+          log.error('csv not captured', err)
+        });
+        await storage
+            .bucket(BUCKET_NAME)
+            .upload(filename)
+            .then(d => log.info('data uploaded to GCP cloud bucket', d))
+            .catch(err => log.error('error uploading the file to gcp', err));
+        break;
+      }
     }
   },
+
   close(options, errors) {
-    fs.unlinkSync('lighthouse.pageSummary.csv');
+    fs.unlinkSync(filename);
   }
 };
