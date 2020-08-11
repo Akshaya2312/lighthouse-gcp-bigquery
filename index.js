48d66be9c1cc2f6ab82b9d905c379bd7ee21ae86
Akshaya Gupta <akshaya@Akshayas-MacBook-Pro.local>
2020-08-11 19:05:58 +0200

    add docker file

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const GOOGLE_CLOUD_PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT_ID;
 const GOOGLE_CLOUD_KEYFILE = process.env.GOOGLE_CLOUD_KEYFILE;
 const BUCKET_NAME = process.env.BUCKET_NAME;
 
+let filename;
+
 const storage = new Storage({
   projectId: GOOGLE_CLOUD_PROJECT_ID,
   keyFilename: GOOGLE_CLOUD_KEYFILE,
 });
-
-const filename = `./pageSummary_${Date.now()}.csv`;
 
 module.exports = {
   concurrency: 1,
@@ -21,30 +21,34 @@ module.exports = {
   },
 
   open(context, options) {
-    this.log = context.intel.getLogger('sitespeedio.plugin.lighthouse-gcp-bigquery');
+    this.log = context.intel.getLogger(
+      'sitespeedio.plugin.lighthouse-gcp-bigquery'
+    );
   },
 
   async processMessage(message, queue) {
+    filename = `./pageSummary_${Date.now()}.csv`;
     const log = this.log;
-    switch(message.type) {
+    switch (message.type) {
       case 'lighthouse.pageSummary': {
-        await converter.json2csvAsync(message).then(csv => {
-          fs.writeFileSync(filename, csv);
-          log.info(`${filename} captured`);
-        }).catch(err => {
-          log.error('csv not captured', err)
-        });
+        await converter
+          .json2csvAsync(message)
+          .then((csv) => {
+            fs.writeFileSync(filename, csv);
+            log.info(`${filename} captured`);
+          })
+          .catch((err) => {
+            log.error('csv not captured', err);
+          });
         await storage
-            .bucket(BUCKET_NAME)
-            .upload(filename)
-            .then(d => log.info('data uploaded to GCP cloud bucket', d))
-            .catch(err => log.error('error uploading the file to gcp', err));
+          .bucket(BUCKET_NAME)
+          .upload(filename)
+          .then((d) => log.info('data uploaded to GCP cloud bucket', d))
+          .catch((err) => log.error('error uploading the file to gcp', err));
         break;
       }
     }
   },
 
-  close(options, errors) {
-    fs.unlinkSync(filename);
-  }
+  close(options, errors) {},
 };
